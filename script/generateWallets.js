000516e8e460550f1206b694ab02fdad1e3db5f3
Vlad Bardadym <vankerbv@gmail.com>
2025-02-11 22:08:45 -0700

    Fixing Bugs in the Code

--- script/generateWallets.js
+++ script/generateWallets.js
@@ -4,17 +4,12 @@
 const web3 = new Web3('https://eth.llamarpc.com');
 
 export function generateWallets() {
-<<<<<<< HEAD
     // console.log('Generating wallets..')
-=======
-    console.log('Generating wallets..')
->>>>>>> c17e620a9a95192217326ff002913e7927886ebb
     const amountOfWallets = document.getElementById('wallets-amount').value;
 
     web3.eth.accounts.wallet.clear();
     let wallets = web3.eth.accounts.wallet.create(amountOfWallets);
 
-<<<<<<< HEAD
     let keys = {};
 
     for (let i = 0; i < wallets.length; i++) {
@@ -34,7 +29,4 @@ export function generateWallets() {
     document.getElementById("private-key-results").innerText = privateKeys;
 
     // console.log(`Here are your wallets public keys: ${wallets[0].address} and private keys ${wallets[0].privateKey}`);
-=======
-    console.log(`Here are your wallets public keys: ${wallets[0].address} and private keys ${wallets[0].privateKey}`);
->>>>>>> c17e620a9a95192217326ff002913e7927886ebb
 }
